e13dd3c5575af72d4307f19315b1bb9ea2d77654
tschokio <tschokio@hotmail.com>
2025-02-22 18:26:56 +0100

    physics

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,34 +1,10 @@
 extends Node2D
 
-var test_array: Array[String] = ["Test", "Hello", "Stuff"]
 
 func _ready():
-	print('level node is ready')
-	$Logo.rotation_degrees = 180
-	
-	for i in test_array:
-		print(i)
+	pass
 
 	
 func _process(delta):
-	$Logo.rotation_degrees += 60 * delta
-	
-	if $Logo.position.x > 1000:
-		$Logo.pos.x = 0
+	pass
 		
-	# exercise
-	
-	if $Logo.visible == true:
-		await try_wait()
-		$Logo.visible = false
-	else:
-		await try_wait()
-		$Logo.visible = true
-		
-func try_wait():
-	await get_tree().create_timer(0.2).timeout
-	
-		
-		
-func test_function():
-	print("this is a test function")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,11 +7,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	
 	# input
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * 500 * delta
+	velocity = direction * 500 
+	move_and_slide()
 	
 	# laser shooting input
 	if Input.is_action_pressed("primary action"):
