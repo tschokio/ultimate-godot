b799d6f3105373dc2f0b42fab86c2f33c472f715
tschokio <tschokio@hotmail.com>
2025-03-01 20:48:46 +0100

    inheritance

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
+var is_enemy: bool = true
+
 func _process(_delta):
 	# direction
 	var direction = Vector2.RIGHT
 	# velocity
-	velocity = direction * 400
+	velocity = direction * 100
 	# move and slide
 	move_and_slide()
+
+func hit():
+	print('damage')
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,4 +1,6 @@
 extends Node2D
+class_name LevelParent
+
 
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
@@ -8,9 +10,7 @@ func _ready():
 func _process(_delta):
 	pass
 		
-func _on_gate_player_entered_gate(_body) -> void:
-	var tween = create_tween()
-	tween.tween_property($Player, "speed", 0 , 0.5)
+
 
 
 func _on_player_player_laser_shot_signal(pos, direction) -> void:
@@ -31,9 +31,9 @@ func _on_player_player_grenade_shot_signal(pos, direction) -> void:
 func _on_house_player_entered() -> void:
 	var tween = get_tree().create_tween()
 	tween.set_parallel(true)
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1).set_trans(Tween.TRANS_QUAD)
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1).set_trans(Tween.TRANS_CIRC)
 	#tween.tween_property($Player, "modulate:a", 0, 2)
 
 func _on_house_player_exited() -> void:
 	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6,0.6), 1)
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6,0.6), 1).set_trans(Tween.TRANS_CIRC)
